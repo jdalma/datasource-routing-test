13e1c4051f1d71dd531054b3911915b05368ac6c
hjjeong <hjjeong@wert.co.kr>
2024-01-20 17:25:19 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/datasourceroutingtest/DatasourceRoutingTestApplication.kt
@@ -0,0 +1,11 @@
+package com.example.datasourceroutingtest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DatasourceRoutingTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<DatasourceRoutingTestApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/datasourceroutingtest/DatasourceRoutingTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.datasourceroutingtest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DatasourceRoutingTestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
